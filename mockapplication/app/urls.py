ab3353632942871814fe523adea38b0d5a24662a
Shaik Masihullah <masihulla17@gmail.com>
2020-12-11 22:09:05 +0530

    Added MockApplication

---
+++ mockapplication/app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import include, path
+from . import views
+
+urlpatterns = [
+	path('contribute/',views.contribute, name="contribute_view"),
+	path('profile',views.userProfile, name="user_profile_view"),
+	path('search',views.search, name="search_view"),
+	path('request/',views.requestpage, name="request_view"),
+]
