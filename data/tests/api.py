1da94122108456ac169c84e05e07f4a52839ad5a
Shaik Masihullah <masihulla17@gmail.com>
2020-12-11 21:26:13 +0530

    Added more tests

--- data/tests/api.py
+++ data/tests/api.py
@@ -7,12 +7,12 @@ from data.models import UserProfile
 from django.utils import timezone
 from django.forms.models import model_to_dict
 
-class TestDownloadView(APITestCase):
+class TestAPIView(APITestCase):
 	def test_anonymous_cannot_access(self):
 		response = self.client.get(reverse("request_dataset_post"))
 		self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-	def test_authenticated_user_can_see_contacts(self):
+	def test_authenticated_user_can_access(self):
 		user = User.objects.create_user("Masihullah," "masihulla17@gmail.com", "abcd1234")
 		token = Token.objects.create(user=user)
 		profile = UserProfile.objects.create(name="Masihullah", email="masihulla17@gmail.com", joined=timezone.now())
@@ -26,5 +26,55 @@ class TestDownloadView(APITestCase):
 		self.assertEqual(response_data['name'], data['name'])
 		self.assertEqual(response_data['email'], data['email'])
 
+	def test_profile_noauth(self):
+		response = self.client.get("/data/api/users/userdoesnotexists/")
+		self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
+	def test_profile(self):
+		user = User.objects.create_user("Masihullah," "masihulla17@gmail.com", "abcd1234")
+		token = Token.objects.create(user=user)
+		profile = UserProfile.objects.create(name="Masihullah", email="masihulla17@gmail.com", joined=timezone.now())
+
+		self.client.credentials(HTTP_AUTHORIZATION="Token " + str(token))
+		response = self.client.get("/data/api/users/userdoesnotexists/")
+		self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+	def test_search_noauth(self):
+		response = self.client.get("/data/api/search/doesnotexists/")
+		self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+	def test_search(self):
+		user = User.objects.create_user("Masihullah," "masihulla17@gmail.com", "abcd1234")
+		token = Token.objects.create(user=user)
+		profile = UserProfile.objects.create(name="Masihullah", email="masihulla17@gmail.com", joined=timezone.now())
+
+		self.client.credentials(HTTP_AUTHORIZATION="Token " + str(token))
+		response = self.client.get("/data/api/search/doesnotexists/")
+		self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+	def test_request_noauth(self):
+		response = self.client.get("/data/api/request/")
+		self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+	def test_request(self):
+		user = User.objects.create_user("Masihullah," "masihulla17@gmail.com", "abcd1234")
+		token = Token.objects.create(user=user)
+		profile = UserProfile.objects.create(name="Masihullah", email="masihulla17@gmail.com", joined=timezone.now())
+
+		self.client.credentials(HTTP_AUTHORIZATION="Token " + str(token))
+		response = self.client.get("/data/api/request/")
+		self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+	def test_contribute_noauth(self):
+		response = self.client.get("/data/api/contribute/")
+		self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+	def test_contribute(self):
+		user = User.objects.create_user("Masihullah," "masihulla17@gmail.com", "abcd1234")
+		token = Token.objects.create(user=user)
+		profile = UserProfile.objects.create(name="Masihullah", email="masihulla17@gmail.com", joined=timezone.now())
+
+		self.client.credentials(HTTP_AUTHORIZATION="Token " + str(token))
+		response = self.client.get("/data/api/contribute/")
+		self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 	
